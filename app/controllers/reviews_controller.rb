b129f55a0098f0d9228475815c7fa557e40b16b1
Maria Terzieva <mariya.g.terzieva@gmail.com>
2017-06-01 18:25:19 +0300

    Fix Reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,10 @@
 class ReviewsController < ApplicationController
   respond_to :html
 
+  before_action :set_book
+
   def index
-    @reviews = Review.order(created_at: :desc).paginate(per_page: 20, page: params[:page])
+    @reviews = @book.reviews.order(created_at: :desc).paginate(per_page: 20, page: params[:page])
     respond_with @reviews
   end
 
@@ -12,13 +14,17 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.create(review_params)
-    respond_with @review
+    @review = Review.create(review_params.merge(book_id: @book.id))
+    respond_with @review, location: book_path(@book)
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:rating, :text, :book)
+    params.require(:review).permit(:rating, :text, :book_id)
+  end
+
+  def set_book
+    @book = Book.find(params[:book_id])
   end
 end
